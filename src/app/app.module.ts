c4340186b98e981303e57fe2e1a08c491834bccb
webmasterdragon101 <webmasterdragon101@gmail.com>
2018-02-19 14:51:50 +0700

    login/signup firebase integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,40 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+// import { Firebase } from "@ionic-native/firebase";
+// import { AngularFireDatabase } from "angularfire2/database";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { HttpModule } from "@angular/http";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { FIREBASE_CONFIG } from "./app.firebase.config";
+import { Storage } from "@ionic/storage";
+import { IonicStorageModule } from "@ionic/storage";
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { FirebaseProvider } from '../providers/firebase/firebase';
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
+import { ClientlocationPage } from '../pages/clientlocation/clientlocation';
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [MyApp, HomePage, LoginPage, SignupPage, ClientlocationPage],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule,
+    HttpModule,
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage, LoginPage, SignupPage, ClientlocationPage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    FirebaseProvider
   ]
 })
 export class AppModule {}
---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,72 @@
+import { LoginPage } from "../login/login";
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  AlertController,
+  Loading,
+  LoadingController,
+  ToastController
+} from "ionic-angular";
+import { User } from "../../models/user";
+import { AngularFireDatabase } from "angularfire2/database";
+import { FirebaseProvider } from "../../providers/firebase/firebase";
+import firebase from "firebase";
+import { Http } from "@angular/http";
+/**
+ * Generated class for the SignupPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: "page-signup",
+  templateUrl: "signup.html"
+})
+export class SignupPage {
+  public user = {} as User;
+  constructor(
+    public http: Http,
+    public afd: AngularFireDatabase,
+    public navCtrl: NavController,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController,
+    public toastCtrl: ToastController,
+    public navParams: NavParams,
+    public firebaseProvider: FirebaseProvider
+  ) {
+    this.http = http;
+  }
+
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad SignupPage");
+  }
+  register() {
+    console.log(this.user);
+    if (this.validateEmail(this.user.email) == false) {
+      let toast = this.toastCtrl.create({
+        message: "Please fill a correct email",
+        duration: 3000,
+        position: "top"
+      });
+      toast.present();
+    } else {
+      this.firebaseProvider.registerUser(this.user);
+       let toast = this.toastCtrl.create({
+         message: "You are registered successfully!",
+         duration: 3000,
+         position: "top"
+       });
+       toast.present();
+      this.navCtrl.push(LoginPage);
+    }
+  }
+
+  validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+}
